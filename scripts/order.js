d337381971befdc015ca2b094d9dc25418080dad
Andrew DGalga <ex.spake@gmail.com>
2024-07-24 08:06:59 -0500

    form pop version1, probably ending here tonight.

--- scripts/order.js
+++ scripts/order.js
@@ -16,12 +16,36 @@ const content = await getCSVContent(orderCSVLocation);
 if(content != ''){
     const rows = makeBlocks(content);
     const bodyRows = rows.slice(1);
+    const formFrag = document.createDocumentFragment();
 
     for(let i = 0; i < bodyRows.length; i++) {
-        bodyRows[i].forEach(elem=>{
+        const field = document.createElement('fieldset');
 
-        })
+        const legend = document.createElement('legend');
+        legend.textContent = bodyRows[i][0];
+        field.append(legend);
+
+        const price = document.createElement('p');
+        price.textContent = 'Price: ' + bodyRows[i][1];
+        field.append(price);
+
+        const amt = document.createElement('input');
+        amt.type = 'number';
+        const forRef = bodyRows[i][0].toLowerCase().replaceAll(' ', '');
+        amt.id = forRef;
+        amt.dataset.price = parseInt(bodyRows[i][1].replace('$',''));
+        amt.min='0';
+        amt.max=`${bodyRows[i][2]}`;
+        amt.value = '0';
+        const amtLabel = document.createElement('label');
+        amtLabel.for = forRef;
+        amtLabel.textContent = `Order? (less than ${bodyRows[i][2]}): `;
+        field.append(amtLabel, amt);
+        
+        formFrag.append(field);
     }
+
+    orderForm.append(formFrag);
 }
 
 orderForm.onsubmit = (e) => {
