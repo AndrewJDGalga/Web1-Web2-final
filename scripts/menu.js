c6e205ee8b5e8332dfb444e3812759d20f757dda
Andrew DGalga <ex.spake@gmail.com>
2024-07-24 03:38:02 -0500

    menujs consts.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,5 @@
 import { getCSVContent, makeBlocks } from "../scripts/read_csv.js";
+const menuTableHead = document.querySelector('#menu thead');
+const menuTableBody = document.querySelector('#menu tbody');
+const menuDataLocation = '../data/menu.csv';
+
