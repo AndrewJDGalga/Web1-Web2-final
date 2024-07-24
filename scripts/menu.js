4226e921d151d3ff6ccd0c67620f8529019370fb
Andrew DGalga <ex.spake@gmail.com>
2024-07-24 03:42:23 -0500

    copied over specialsjs code with minimal touches. Works for injecting 3 cols but ugly and will get unweildy fast. TODO: decide->rebuild only for menu OR could there be some shared functions?

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,48 @@
 import { getCSVContent, makeBlocks } from "../scripts/read_csv.js";
 const menuTableHead = document.querySelector('#menu thead');
 const menuTableBody = document.querySelector('#menu tbody');
-const menuDataLocation = '../data/menu.csv';
+const menuCSVLocation = '../data/menu.csv';
 
+const content = await getCSVContent(menuCSVLocation);
+
+if(content !== '') {
+    const rows = makeBlocks(content);
+    const headRow = rows[0];
+    const bodyRows = rows.slice(1);
+
+    const headTR = document.createElement('tr');
+    headRow.forEach(item=>{
+        const headCol = document.createElement('th');
+        headCol.textContent = item;
+        headTR.appendChild(headCol);
+    })
+    menuTableHead.appendChild(headTR);
+
+    const bodyFrag = document.createDocumentFragment();
+    bodyRows.forEach(row=>{
+        const bodTR = document.createElement('tr');
+        
+        const description = document.createElement('p');
+        const descriptionCell = document.createElement('td');
+        const img = document.createElement('img');
+        const imgCell = document.createElement('td');
+        const date = document.createElement('p');
+        const dateCell = document.createElement('td');
+
+        description.textContent = row[0];
+        img.src = `../img/${row[1]}`;
+        const leadingZero = /0(?=\d)/g;
+        const correctedDate = row[2].replace(leadingZero, '');
+
+        date.textContent = correctedDate;
+
+        descriptionCell.append(description);
+        imgCell.append(img);
+        dateCell.append(date);
+        
+        bodTR.append(descriptionCell, imgCell, dateCell);
+        bodyFrag.append(bodTR);
+    });
+    
+    menuTableBody.append(bodyFrag);
+}
