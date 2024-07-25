048114d24f4fab1e11083e26e86ecc5079e7590d
Andrew DGalga <ex.spake@gmail.com>
2024-07-25 18:54:11 -0500

    updated folder structure per requirements.

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,15 +1,15 @@
 const adBody = document.getElementById('ad');
 const adImg = document.getElementsByClassName('ad-background');
 const imageLocations = [
-    '../img/ad_images/alexander-grey-ZYyBlwUFPFk-unsplash-small.jpg',
-    '../img/ad_images/analia-baggiano-CdnyQH3_Lb4-unsplash-small.jpg',
-    '../img/ad_images/dana-devolk-5-RS_ScO3X4-unsplash-small.jpg',
-    '../img/ad_images/dylan-ferreira-gHXi8mBHPyM-unsplash-small.jpg',
-    '../img/ad_images/josephina-kolpachnikof-piA65cp-1EI-unsplash-small.jpg',
-    '../img/ad_images/patrick-fore-AfhSPYdkxiU-unsplash-small.jpg',
-    '../img/ad_images/rachael-gorjestani-HLt6jQLf_J0-unsplash-small.jpg',
-    '../img/ad_images/robert-anasch-ugV_7jiFRxM-unsplash-small.jpg',
-    '../img/ad_images/yes-and-studio-XVYz_QeiEBw-unsplash-small.jpg'
+    '../img/images/alexander-grey-ZYyBlwUFPFk-unsplash-small.jpg',
+    '../img/images/analia-baggiano-CdnyQH3_Lb4-unsplash-small.jpg',
+    '../img/images/dana-devolk-5-RS_ScO3X4-unsplash-small.jpg',
+    '../img/images/dylan-ferreira-gHXi8mBHPyM-unsplash-small.jpg',
+    '../img/images/josephina-kolpachnikof-piA65cp-1EI-unsplash-small.jpg',
+    '../img/images/patrick-fore-AfhSPYdkxiU-unsplash-small.jpg',
+    '../img/images/rachael-gorjestani-HLt6jQLf_J0-unsplash-small.jpg',
+    '../img/images/robert-anasch-ugV_7jiFRxM-unsplash-small.jpg',
+    '../img/images/yes-and-studio-XVYz_QeiEBw-unsplash-small.jpg'
 ];
 
 const randomImageString = () => {
