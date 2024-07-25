93093db5371c7503f4b6552089ced7eb1d95e2f2
Andrew DGalga <ex.spake@gmail.com>
2024-07-25 01:44:02 -0500

    base ad styling, will need replacement. Manually loading images via array.

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,4 +1,15 @@
 const adBody = document.getElementById('ad');
 const adImg = document.getElementsByClassName('ad-background');
-const imagesFolder = '../img/ad_images';
+const imageLocations = [
+    '../img/ad_images/alexander-grey-ZYyBlwUFPFk-unsplash-small.jpg',
+    '../img/ad_images/analia-baggiano-CdnyQH3_Lb4-unsplash-small.jpg',
+    '../img/ad_images/dana-devolk-5-RS_ScO3X4-unsplash-small.jpg',
+    '../img/ad_images/dylan-ferreira-gHXi8mBHPyM-unsplash-small.jpg',
+    '../img/ad_images/josephina-kolpachnikof-piA65cp-1EI-unsplash-small.jpg',
+    '../img/ad_images/patrick-fore-AfhSPYdkxiU-unsplash-small.jpg',
+    '../img/ad_images/rachael-gorjestani-HLt6jQLf_J0-unsplash-small.jpg',
+    '../img/ad_images/robert-anasch-ugV_7jiFRxM-unsplash-small.jpg',
+    '../img/ad_images/yes-and-studio-XVYz_QeiEBw-unsplash-small.jpg'
+];
+
 
